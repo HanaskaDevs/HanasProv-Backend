ef1042d10ddc622a0ead1056da917c71e510e436
Isak-Flores <mateofm8@hotmail.com>
2026-07-08 11:16:11 -0500

    FichaProveedor FlujoV1

--- app/Modules/Auth/Models/Usuario.php
+++ app/Modules/Auth/Models/Usuario.php
@@ -61,6 +61,18 @@ class Usuario extends Authenticatable
     }
 
     /**
+     * El trait Notifiable, por defecto, busca $this->email (minúscula) para
+     * saber a dónde mandar notificaciones por el canal "mail". Nuestra
+     * columna real es "Email" (con mayúscula) -> sin este método, Eloquent
+     * nunca encuentra el destinatario y el correo se "envía" sin ir a
+     * ningún lado, sin lanzar ninguna excepción visible.
+     */
+    public function routeNotificationForMail(\Illuminate\Notifications\Notification $notification): string
+    {
+        return $this->Email;
+    }
+
+    /**
      * ¿Este usuario tiene el rol indicado (por nombre) en la empresa dada?
      * El vínculo Usuario_Empresa debe estar activo.
      */
